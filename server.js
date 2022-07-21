2bc5a3233f2196f0ab0daa3a06c288437fcfe33e
jfranklin12 <j.franks725@gmail.com>
2022-07-21 12:42:44 -0400

    worked toward displaying notes to screen throught GET api/notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 4001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -10,33 +10,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+// GET request for index.html
 app.get('/', (req, res) => {
   console.info(`${req.method} request to receive to get index.html`)
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
+// GET request for notes html
 app.get('/notes', (req, res) => {
   console.info(`${req.method} request received to get notes`);
   res.sendFile(path.join(__dirname, '/public/notes.html'));
-})
+});
+// GET request for notes JSON
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request to get JSON notes received`);
+  res.json(`${req.method} request to get JSON notes received`);
+});
 
-// Read json file and allow new notes to be created in the json file h
+
+// Read json file and allow new notes to be created in the json file 
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
-  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
-  // If all the required properties are present
+  // Create note if all the propertiies are present
   if (title && text) {
-    // Variable for the object we will save
     const newNote = {
       title,
       text,
     };
 
-    // Obtain existing reviews and convert string into JSON object
+    // Obtain created notes and covert string to JSON object
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
@@ -46,16 +50,14 @@ app.post('/api/notes', (req, res) => {
         // Add a new note
         parsedNotes.push(newNote);
 
-        // Write updated reviews back to the file
+        // Write updated notes back to the file
         fs.writeFile(
           './db/db.json',
           JSON.stringify(parsedNotes, null, 2),
           (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
+            writeErr ? console.error(writeErr) : console.info('Successfully updated notes!')
         );
-      }
+      };
     });
 
     const response = {
@@ -69,7 +71,6 @@ app.post('/api/notes', (req, res) => {
     res.status(500).json('Error in posting note');
   }
 });
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
